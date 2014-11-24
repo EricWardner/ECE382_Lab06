af3574f0478099408f80a9e9e0b43015c9da7b26
unknown <C16Eric.Wardner@c16ejwardner.usafa.ds.af.edu>
2014-11-24 08:39:30 -0700

    forward and back, some delay issues

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <msp430.h>
+void forward();
+void back();
+
+
+void main(void) {
+	forward();
+	_delay_cycles(100000);
+	back();
+	_delay_cycles(100000);
+
+
+} // end main
+
+void forward(){
+    WDTCTL = WDTPW|WDTHOLD;                 // stop the watchdog timer
+
+    P2DIR |= BIT2;							// P2.2 is associated with TA1CCR1
+    P2SEL |= BIT2;							// P2.2 is associated with TA1CCTL1
+
+    P2DIR |= BIT4;							// P2.4 is associated with TA1CCR2
+    P2SEL |= BIT4;							// P2.4 is associated with TA1CCTL2
+
+    P2DIR |= BIT1|BIT3;							// P2.4 is associated with TA1CCR2
+    P2OUT |= BIT1|BIT3;
+
+	TA1CTL = ID_3 | TASSEL_2 | MC_1;		// Use 1:8 presclar off MCLK
+    TA1CCR0 = 0x0100;						// set signal period
+
+    TA1CCR1 = 0x0020;
+    TA1CCTL1 = OUTMOD_7;					// set TACCTL1 to Reset / Set mode
+
+    TA1CCR2 = 0x0020;
+    TA1CCTL2 = OUTMOD_7;					// set TACCTL1 to Reset / Set mode
+}
+
+void back(){
+    WDTCTL = WDTPW|WDTHOLD;                 // stop the watchdog timer
+
+    P2DIR |= BIT2;							// P2.2 is associated with TA1CCR1
+    P2SEL |= BIT2;							// P2.2 is associated with TA1CCTL1
+
+    P2DIR |= BIT4;							// P2.4 is associated with TA1CCR2
+    P2SEL |= BIT4;							// P2.4 is associated with TA1CCTL2
+
+    P2DIR |= BIT1|BIT3;							// P2.4 is associated with TA1CCR2
+    P2OUT &= ~(BIT1|BIT3);
+
+	TA1CTL = ID_3 | TASSEL_2 | MC_1;		// Use 1:8 presclar off MCLK
+    TA1CCR0 = 0x0100;						// set signal period
+
+    TA1CCR1 = 0x0020;
+    TA1CCTL1 = OUTMOD_3;					// set TACCTL1 to Reset / Set mode
+
+    TA1CCR2 = 0x0020;
+    TA1CCTL2 = OUTMOD_3;					// set TACCTL1 to Reset / Set mode
+}
+
+
+
